ed5f30ab09e2ceb8cd1e23342d1c81461990f040
mraviel <mraviel@outlook.com>
2019-02-08 12:28:18 +0200

    add the squreLocation function: find the location of the speciphic squre.

--- board.py
+++ board.py
@@ -20,16 +20,16 @@ class Board():
 
         board = [[None] * 8 for i in range(8)]
 
-        for y in range(8):
-            for x in range(8):
+        for x in range(8):
+            for y in range(8):
                 if (x % 2 != 0) and (y % 2 == 0):
-                    board[y][x] = Squre(WHITE)
+                    board[x][y] = Squre(WHITE, self.squreLocation(x, y))
                 elif (x % 2 == 0) and (y % 2 != 0):
-                    board[y][x] = Squre(WHITE)
+                    board[x][y] = Squre(WHITE, self.squreLocation(x, y))
                 elif (x % 2 != 0) and (y % 2 != 0):
-                    board[y][x] = Squre(BLACK)
+                    board[x][y] = Squre(BLACK, self.squreLocation(x, y))
                 elif (x % 2 == 0) and (y % 2 == 0):
-                    board[y][x] = Squre(BLACK)
+                    board[x][y] = Squre(BLACK, (int(920 / 8 * x + 50),int(920 / 8 * y + 50)))
 
 
         print(board)
@@ -39,6 +39,35 @@ class Board():
 
     def drawBoardGame(self, screen, board):
 
+        """ Draw the all board on the screen """
+
         for y in range(8):
             for x in range(8):
                 pg.draw.rect(screen, self.board[x][y].color, (WIDTH / 8 * x, HEIGHT / 8 * y, WIDTH / 8, HEIGHT / 8), 0)
+
+        self.drawPiecesOnBoard(screen)
+
+
+    def drawPiecesOnBoard(self, screen):
+
+        """ Draw the Pieces (players) on the board \ screen """
+
+        # pg.draw.circle(screen, RED, self.board[5][1].pos, 40)
+
+        for x in range(8):
+            for y in range(8):
+                pass
+
+    def middelSqure(self):
+
+        """ Get the middel of the squre.  Return: tupple of the middel location """
+
+        pass
+
+    def squreLocation(self, x, y):
+
+        """ Get the squre location.  Argumentes: x, y of the location(min(0,0), max(7,7)).
+            Return: tupple of the location """
+
+        return (int(920 / 8 * x + 50), int(920 / 8 * y + 50))
+
--- squre.py
+++ squre.py
@@ -2,6 +2,7 @@
 
 class Squre():
 
-    def __init__(self, color):
+    def __init__(self, color, pos):
 
         self.color = color  # the color of the squre (black or white)
+        self.pos = pos
